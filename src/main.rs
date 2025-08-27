c9589ca27e80d4087c0f67e8717e340797fd6f1a
thomsva <thomsva@gmail.com>
2025-08-27 12:33:09 +0300

    continuous discovery implemented

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
-use bluer::{AdapterEvent, Address};
+use bluer::{Adapter, AdapterEvent, Address, DiscoveryFilter, DiscoveryTransport};
 use futures_util::stream::StreamExt;
 
-use std::collections::HashSet;
-
-use tokio::time::{self, Duration};
+use std::collections::{HashMap, HashSet};
 
 mod decode_ruuvi; // declares the module
 use decode_ruuvi::decode_ruuvi_raw5; // imports the function
@@ -16,15 +14,57 @@ use mqtt::MqttHandler;
 
 mod startup_info;
 use startup_info::print_startup_info;
+const RUUVI_COMPANY_ID: u16 = 0x0499;
+
+async fn extract_ruuvi_payload(
+    adapter: &Adapter,
+    mac: &Address,
+    config: &config::Config,
+) -> Option<Vec<u8>> {
+    let device = adapter.device(*mac).ok()?; // return None if failed
+    let blocked_by_whitelist =
+        config.sensors.use_whitelist && !config.sensors.whitelist.contains(&mac);
+    let blocked_by_blacklist =
+        config.sensors.use_blacklist && config.sensors.blacklist.contains(&mac);
+    let allowed = !blocked_by_whitelist && !blocked_by_blacklist;
+
+    if !allowed && config.sensors.debug_print {
+        if blocked_by_whitelist {
+            println!("{} → blocked by whitelist", mac);
+        } else if blocked_by_blacklist {
+            println!("{} → blocked by blacklist", mac);
+        }
+    }
+
+    if !allowed {
+        return None;
+    }
+
+    let data = device.manufacturer_data().await.ok()??; // double ? for Option inside Result
+    data.get(&RUUVI_COMPANY_ID).cloned() // return Some(payload) or None
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let config = load_config()?;
     // println!("{:#?}", config);
-
     let session = bluer::Session::new().await?;
-    let adapter = session.default_adapter().await?;
+
+    //let adapter = session.default_adapter().await?;
+    let adapter = match session.default_adapter().await {
+        Ok(a) => a,
+        Err(e) => {
+            eprintln!("❌ No default Bluetooth adapter found: {}", e);
+            std::process::exit(1);
+        }
+    };
     adapter.set_powered(true).await?;
+    let filter = DiscoveryFilter {
+        transport: DiscoveryTransport::Le,
+        discoverable: false,
+        ..Default::default()
+    };
+    adapter.set_discovery_filter(filter).await?;
 
     print_startup_info(&config, &adapter).await;
 
@@ -38,92 +78,66 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     )
     .await;
 
+    let mut events = adapter.discover_devices_with_changes().await?;
+    let mut previous_payload: HashMap<Address, Vec<u8>> = HashMap::new();
     let mut known_sensors: HashSet<Address> = HashSet::new();
 
-    loop {
-        let mut events = adapter.discover_devices().await?;
+    while let Some(event) = events.next().await {
+        match event {
+            AdapterEvent::DeviceAdded(mac) => {
+                // Get Ruuvi payload if possible
+                let Some(payload) = extract_ruuvi_payload(&adapter, &mac, &config).await else {
+                    continue;
+                };
 
-        while let Ok(Some(AdapterEvent::DeviceAdded(addr))) =
-            time::timeout(Duration::from_secs(2), events.next()).await
-        {
-            // let mac = addr.to_string();
-
-            // Device object based on addr
-            let device = match adapter.device(addr) {
-                Ok(d) => d,
-                Err(_) => continue,
-            };
-
-            // Extract manufacturer data
-            let manuf = match device.manufacturer_data().await {
-                Ok(Some(m)) => m,
-                _ => continue,
-            };
-
-            let data = match manuf.get(&1177) {
-                Some(d) => d,
-                None => continue,
-            };
-
-            // Determine if sensor is allowed by blacklist and whitelist rules
-            let blocked_by_whitelist =
-                config.sensors.use_whitelist && !config.sensors.whitelist.contains(&addr);
-            let blocked_by_blacklist =
-                config.sensors.use_blacklist && config.sensors.blacklist.contains(&addr);
-            let allowed = !blocked_by_whitelist && !blocked_by_blacklist;
-
-            if !allowed && config.sensors.debug_print {
-                if blocked_by_whitelist {
-                    println!("{} → blocked by whitelist", addr);
-                } else if blocked_by_blacklist {
-                    println!("{} → blocked by blacklist", addr);
+                // Remove duplicates
+                if previous_payload.get(&mac) == Some(&payload) {
+                    continue;
                 }
-            }
+                previous_payload.insert(mac.clone(), payload.clone());
 
-            if !allowed {
-                continue; // Skip further processing
-            }
-
-            // Check if new sensor
-            if !known_sensors.contains(&addr) {
-                println!("New Ruuvi sensor detected and saved: {}", addr);
-                known_sensors.insert(addr.clone());
-                if config.publish.discovery {
-                    mqtt.send_discovery(&addr.to_string()).await.ok();
-                }
-            }
-
-            if config.publish.raw_data {
-                if let Err(e) = mqtt.publish_raw(&addr.to_string(), &data).await {
-                    eprintln!("❌ Failed to publish raw data for {}: {}", addr, e);
-                }
-            }
-
-            match decode_ruuvi_raw5(&data) {
-                Some((t, h, p, b)) => {
-                    // Always print if debug_print_measurements is enabled
-                    if config.sensors.debug_print {
-                        println!("{} → {:.2}°C  {:.1}%  {:.1}hPa  {:.3}V", addr, t, h, p, b);
+                // Publish discovery if new sensor
+                if !known_sensors.contains(&mac) {
+                    println!("New Ruuvi sensor detected and saved: {}", mac);
+                    known_sensors.insert(mac.clone());
+                    if config.publish.discovery {
+                        mqtt.send_discovery(&mac.to_string()).await.ok();
                     }
+                }
 
-                    // Publish if enabled
-                    if config.publish.decoded_data {
-                        if let Err(e) = mqtt.publish_decoded(&addr, t, h, p).await {
-                            eprintln!("❌ Failed to publish decoded data for {}: {}", addr, e);
+                // Publish raw data
+                if config.publish.raw_data {
+                    if let Err(e) = mqtt.publish_raw(&mac.to_string(), &payload).await {
+                        eprintln!("❌ Failed to publish raw data for {}: {}", mac, e);
+                    }
+                }
+
+                // Publish decoded data
+                match decode_ruuvi_raw5(&payload) {
+                    Some((t, h, p, b)) => {
+                        // Always print if debug_print_measurements is enabled
+                        if config.sensors.debug_print {
+                            println!("{} → {:.2}°C  {:.1}%  {:.1}hPa  {:.3}V", mac, t, h, p, b);
+                        }
+
+                        // Publish if enabled
+                        if config.publish.decoded_data {
+                            if let Err(e) = mqtt.publish_decoded(&mac, t, h, p).await {
+                                eprintln!("❌ Failed to publish decoded data for {}: {}", mac, e);
+                            }
+                        }
+                    }
+                    None => {
+                        if config.sensors.debug_print {
+                            eprintln!("⚠️ Failed to decode Ruuvi data from {}", mac);
                         }
                     }
                 }
-                None => {
-                    if config.sensors.debug_print {
-                        eprintln!("⚠️ Failed to decode Ruuvi data from {}", addr);
-                    }
-                }
             }
+
+            AdapterEvent::DeviceRemoved(_mac) => {}
+            AdapterEvent::PropertyChanged(_p) => {}
         }
-
-        drop(events);
-
-        // Wait 10 seconds before next discovery cycle
-        tokio::time::sleep(Duration::from_secs(10)).await;
     }
+    Ok(())
 }
